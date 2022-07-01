d56694c438b4151d652176708155e1efcc24757b
Minosuke1219 <93524736+JanoAngulo@users.noreply.github.com>
2022-07-02 00:45:10 +0800

    Added Firebase Hosting

--- src/Requests.js
+++ src/Requests.js
@@ -1,9 +1,9 @@
-const key = '184c2be0e99a7dc468a51f1800c1d894';
+const key = "184c2be0e99a7dc468a51f1800c1d894";
 const requests = {
-  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-  requestLatest: `https://api.themoviedb.org/3/movie/latest?api_key=${key}&language=en-US`,
-  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
-}
+	requestPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+	requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+	requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+	requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+};
 
 export default requests;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Main from '../components/Main'
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import request from "../Requests";
 
 const Home = () => {
-  return (
-    <>
-      <Main/>
-    </>
-  )
-}
+	return (
+		<>
+			<Main />
+      <Row rowID='1' title="Now Playing" fetchURL={request.requestPlaying} />
+			<Row rowID='2' title="Up Coming" fetchURL={request.requestUpcoming} />
+			<Row rowID='3' title="Top Rated" fetchURL={request.requestTopRated} />
+			<Row rowID='4' title="Popular" fetchURL={request.requestPopular} />
+		</>
+	);
+};
 
-export default Home
+export default Home;
